2400fd124f9d416f55535918777e5c7f34d02e6c
Dhruv  Sringari <dhruvy427@gmail.com>
2017-10-09 14:45:48 -0400

    Refactors bluetooth status and programs move button

--- ARBot/ARBot/ViewController.swift
+++ ARBot/ARBot/ViewController.swift
@@ -10,28 +10,62 @@ import UIKit
 import CoreBluetooth
 
 class ViewController: UIViewController {
+    enum BluetoothStatus {
+        case off
+        case searching
+        case connected
+        case stopped
+        case unknown
+    }
+    
     var serial: BluetoothSerial!
     @IBOutlet var bluetoothStatusLabel: UILabel!
+    var bluetoothStatus: BluetoothStatus = .off
     @IBOutlet var motorSpeedLabel: UILabel!
+    let stopSpeed: Int = 94
 
     override func viewDidLoad() {
         super.viewDidLoad()
         serial = BluetoothSerial(delegate: self)
         serial.startScan()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
-        changeMotorSpeed(speed: Int(sender.value))
+        changeServoSpeed(speed: Int(sender.value))
     }
     
-    func changeMotorSpeed(speed: Int) {
+    func changeServoSpeed(speed: Int) {
         serial.sendMessageToDevice("setSpeed:\(speed)\n")
         motorSpeedLabel.text = "\(speed)"
+    }
+    
+    @IBAction func movePressedDown(_ sender: Any) {
+        changeServoSpeed(speed: 180)
+    }
+    
+    @IBAction func moveUnpressed(_ sender: Any) {
+        changeServoSpeed(speed: stopSpeed)
+    }
+    
+    
+    func setBluetooth(status: BluetoothStatus) {
+        switch status {
+        case .off:
+            bluetoothStatusLabel.text = "Bluetooth Off"
+            bluetoothStatusLabel.textColor = UIColor.red
+        case .searching:
+            bluetoothStatusLabel.text = "Bluetooth Searching..."
+            bluetoothStatusLabel.textColor = UIColor.orange
+        case .connected:
+            bluetoothStatusLabel.text = "Connected"
+            bluetoothStatusLabel.textColor = UIColor.green
+        case .stopped:
+            bluetoothStatusLabel.text = "Bluetooth Disconnected and Not Searching"
+            bluetoothStatusLabel.textColor = UIColor.red
+        case .unknown:
+            bluetoothStatusLabel.text = "Unknown"
+            bluetoothStatusLabel.textColor = UIColor.red
+        }
     }
     
 }
@@ -40,27 +74,23 @@ extension ViewController: BluetoothSerialDelegate {
     func serialDidChangeState() {
         switch serial.centralManager.state {
         case .poweredOff:
-            bluetoothStatusLabel.text = "Bluetooth Off"
-            bluetoothStatusLabel.textColor = UIColor.red
+            setBluetooth(status: .off)
         case .poweredOn:
-            bluetoothStatusLabel.text = "Bluetooth Searching..."
-            bluetoothStatusLabel.textColor = UIColor.orange
+            setBluetooth(status: .searching)
             serial.startScan()
         default:
-            bluetoothStatusLabel.text = "Unknown"
-            bluetoothStatusLabel.textColor = UIColor.orange
+            setBluetooth(status: .unknown)
         }
     }
     
     func serialDidDisconnect(_ peripheral: CBPeripheral, error: NSError?) {
-        
+       setBluetooth(status: .stopped)
     }
     
     func serialDidDiscoverPeripheral(_ peripheral: CBPeripheral, RSSI: NSNumber?) {
         if peripheral.name == "MLT-BT05" {
             serial.connectToPeripheral(peripheral)
-            bluetoothStatusLabel.text = "Connected"
-            bluetoothStatusLabel.textColor = UIColor.green
+            setBluetooth(status: .connected)
         }
     }
     
